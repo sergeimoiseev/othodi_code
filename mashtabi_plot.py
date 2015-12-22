1f60dea3407d6864b7d6fb8056e6dc45efd3b3d3
Sergei <Sergei>
2015-12-22 17:57:46 +0300

    good png generated, params changed

--- mashtabi_plot.py
+++ mashtabi_plot.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-from transliterate import translit
 import matplotlib.cm as cm
 
 ## Installed LaTeX needed for cyrilic text
@@ -10,21 +9,20 @@ import matplotlib.cm as cm
 #         'weight': 'normal',
 #         'size': 14}
 
-print(24*265)
+from transliterate import translit
+def tr_ru(str_):
+	trans_str = translit(str_,'ru',reversed=True)
+	return trans_str
 
 fig = plt.figure("caption",figsize=(10,10))
 ax = fig.add_subplot(111)
-title_text = translit(u"Характерные масштабы задачи управления распределением отходов",'ru',reversed=True)
+title_text = tr_ru(u"Характерные масштабы задачи управления распределением отходов")
 ax.set_title(title_text)
-ax.set_xlabel(translit(u'Время, ч','ru',reversed=True))
-ax.set_ylabel(translit(u'Вес, кг','ru',reversed=True))
-xy_pairs = [(1,1),(1e3,1),(1e3,10),(1e3,100),(1e3,1e5)]
-
-
-# colors = list(cm.rainbow(np.linspace(0, 1, len(xy_pairs))))
-# for y, c in zip(xy_pairs, colors):
-#     plt.scatter(x, y, color=c)
-# print(colors)
+ax.set_xlabel(tr_ru(u'Время, ч'))
+ax.set_ylabel(tr_ru(u'Вес, кг'))
+xy_pairs = [(1,1),(1,10),(1e1,10),(1e2,100),(1e3,1e5)]
+annotations=[u"Масштаб\nотходов",u"Масштаб\nперевозок",u"Масштаб\nпроизводственных\nсмен",u"Масштаб\nпроизводственных\nциклов",u"Масштаб\nпланирования\nпроизводства"]
+annotations = [tr_ru(ann) for ann in annotations]
 point_size = 50
 
 import itertools
@@ -36,24 +34,19 @@ def pairwise(iterable):
 
 color_iter_lines=iter(cm.rainbow(np.linspace(0,1,len(xy_pairs))))
 color_iter_points=iter(cm.rainbow(np.linspace(0,1,len(xy_pairs))))
-# color=iter(cm.rainbow(np.linspace(0,1,len(xy_pairs))))
 for points_pair in pairwise(xy_pairs):
-# for points_pair,col in zip(pairwise(xy_pairs), colors[0:len(xy_pairs)-1]):
 	x_list = [points_pair[0][0],points_pair[1][0]]
 	y_list = [points_pair[0][1],points_pair[1][1]]
-	# ax.loglog(x_list,y_list,ls='-',lw=point_size,alpha=0.5,solid_capstyle='round',color=col)
 	c=next(color_iter_lines)
 	ax.loglog(x_list,y_list,ls='-',lw=point_size,alpha=0.5,solid_capstyle='round',color=c)
-	# line.set_solid_capstyle('round')
 
-# color_iter=iter(['r','g','b','c','m',])
-# colors1 = list(cm.rainbow(np.linspace(0, 1, len(xy_pairs))))
 for pair in xy_pairs:
-# for pair,col in zip(xy_pairs, colors1[0:len(xy_pairs)]):
 	c=next(color_iter_points)
 	print("color = %s" % c)
 	ax.loglog(pair[0],pair[1],marker='o',ms=point_size,mec='None',alpha=1,color=c)
-	# ax.loglog(pair[0],pair[1],marker='o',ms=point_size,mfc=(1.,0.,0.,0.5),mec='None',alpha=1,color =col)
+
+for i, txt in enumerate(annotations):
+    ax.annotate(txt, (xy_pairs[i][0],xy_pairs[i][1]))
 
 y_sublist = [xy_pairs[i][1] for i in range(len(xy_pairs))]
 x_sublist = [xy_pairs[i][0] for i in range(len(xy_pairs))]
