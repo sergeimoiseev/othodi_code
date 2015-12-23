15feb61486b3fb2dbfec77f89c38aea7fe352f6f
Sergei <Sergei>
2015-12-23 17:16:37 +0300

    added othodi.py, added plot_routes.py adn got the working key for gmaps directions API!

---
+++ plot_routes.py
@@ -0,0 +1,7 @@
+ # -*- coding: utf-8 -*-
+ # 55.7522200 широта
+ # 37.6155600 долгота
+ # working google key for google maps directions API!
+ # AIzaSyDhefiliHi_T2eke5NRHzKWvGqj7OteDog
+ # example request 
+ # https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyDhefiliHi_T2eke5NRHzKWvGqj7OteDog
