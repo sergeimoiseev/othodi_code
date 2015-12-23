c6c0f759f9314b6a7771d2c99fcefed87f407957
Sergei <moiseev.sergei@gmail.com>
2015-12-23 22:37:04 +0300

    found duration and distance

--- plot_routes.py
+++ plot_routes.py
@@ -6,40 +6,59 @@
  # example request 
  # https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyDhefiliHi_T2eke5NRHzKWvGqj7OteDog
 
-test_json_url = "https://maps.googleapis.com/maps/api/directions/xml?origin=Toronto&destination=Montreal&key=AIzaSyDhefiliHi_T2eke5NRHzKWvGqj7OteDog"
+import json, requests#, ittertools
+
+url = 'https://maps.googleapis.com/maps/api/directions/json'
+
+params = dict(
+    origin='Toronto',
+    destination='Montreal',
+    # waypoints='Joplin,MO|Oklahoma+City,OK',
+    # sensor='false'
+    key='AIzaSyDhefiliHi_T2eke5NRHzKWvGqj7OteDog'
+)
+
+resp = requests.get(url=url, params=params)
+data = json.loads(resp.text)
+
+# print(data)
+# print(data['distance'])
+# for key,value in data.keys():
+#     print(key)
+#     for k,v in value
+
+def pretty(d, indent=0):
+   for key, value in d.iteritems():
+      print '\t' * indent + str(key)
+      if isinstance(value, dict):
+         pretty(value, indent+1)
+      else:
+         print '\t' * (indent+1) + str(value)
+# pretty(data,  indent=1)
+
+routes = data["routes"]
+print(len(routes[0]))    
+print(type(routes[0]))
+for key, val in routes[-1].iteritems():
+    print(key)
+# print("routes[0]['bounds']")    
+# print(routes[0]['bounds']) 
+# print("routes[0]['legs']")    
+# print(routes[0]['legs'])
+# print(type(routes[0]['legs'][-1]))
+# print(len(routes[0]['legs'][-1]))
+# for key,val in (routes[0]['legs'][-1]).iteritems():
+#     print(key, len(val),type(val))
+
+print(routes[0]['legs'][-1][u'duration'])
+print(routes[0]['legs'][-1][u'distance'])
+# test_json_url = "https://maps.googleapis.com/maps/api/directions/xml?origin=Toronto&destination=Montreal&key=AIzaSyDhefiliHi_T2eke5NRHzKWvGqj7OteDog"
 # test_url = "https://maps.googleapis.com/maps/api/directions/xml?origin=Toronto&destination=Montreal&key=AIzaSyDhefiliHi_T2eke5NRHzKWvGqj7OteDog"
-import urllib
-answer = urllib.urlopen(test_url).read()
-# print(doc[:350])
+# import urllib
+# answer = urllib.urlopen(test_json_url)
+# answer = urllib.urlopen(test_json_url).read()
+# print(answer)
+# json_ans=json.loads(answer.text)
+# for i in json_ans:
+    # print(i)
 
-from bs4 import BeautifulSoup, SoupStrainer
-line=answer
-# line='<City_State>PLAINSBORO, NJ 08536-1906</City_State>'
-soup = BeautifulSoup(line,'lxml')
-# print(soup.find('distance').text)
-# print(soup.find('distance').text)
-
-def find_total_dur_dist_in_soup(soup_obj):
-    # for duration in 
-    distance_list = soup_obj.find_all('distance')
-    duration_list = soup_obj.find_all('duration')
-    total_duration, total_distance = max(duration_list), max(distance_list)
-    return total_duration, total_distance
-print(find_total_dur_dist_in_soup(soup))
-
-# print(soup.find('duration').text)
-# print(soup.find('value').text)
-# print(soup.find('city_state').text)
-# print(soup)
-# for link in BeautifulSoup(answer, parseOnlyThese=SoupStrainer('text')):
-#     print(link)
-    # if link.has_attr('href'):
-    #     print link['href']
-
-# import re
-# def parse_get_treavel_time(urllib_read_data):
-#     groups = re.search('>.*<', urllib_read_data)
-#     return groups
-    # return treaver_time
-# list_of_things_between_tags = parse_get_treavel_time(answer)
-# print(list_of_things_between_tags)
