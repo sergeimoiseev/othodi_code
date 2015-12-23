4c4e57c0011647d90b9bdae2530e561b2bb7631d
Sergei <moiseev.sergei@gmail.com>
2015-12-23 23:55:58 +0300

    nearly plot in plot_routes.py, only plot command remaining

--- plot_routes.py
+++ plot_routes.py
@@ -10,11 +10,18 @@ import json, requests#, ittertools
 
 url = 'https://maps.googleapis.com/maps/api/directions/json'
 
+origin = {'latt':55.7522200,'long':37.6155600}
+dest = {'latt':59.9386300,'long':30.3141300}
+
 params = dict(
-    origin='Toronto',
-    destination='Montreal',
+    # origin='Toronto',
+    origin='%f,%f' % (origin["latt"],origin["long"]),
+    # origin='55.7522200,37.6155600',
+    # destination='Montreal',
+    destination='%f,%f' % (dest["latt"],dest["long"]),
+    # destination='59.9386300,30.3141300',
     # waypoints='Joplin,MO|Oklahoma+City,OK',
-    # sensor='false'
+    sensor='false',
     key='AIzaSyDhefiliHi_T2eke5NRHzKWvGqj7OteDog'
 )
 
@@ -52,13 +59,22 @@ for key, val in routes[-1].iteritems():
 
 print(routes[0]['legs'][-1][u'duration'])
 print(routes[0]['legs'][-1][u'distance'])
-# test_json_url = "https://maps.googleapis.com/maps/api/directions/xml?origin=Toronto&destination=Montreal&key=AIzaSyDhefiliHi_T2eke5NRHzKWvGqj7OteDog"
-# test_url = "https://maps.googleapis.com/maps/api/directions/xml?origin=Toronto&destination=Montreal&key=AIzaSyDhefiliHi_T2eke5NRHzKWvGqj7OteDog"
-# import urllib
-# answer = urllib.urlopen(test_json_url)
-# answer = urllib.urlopen(test_json_url).read()
-# print(answer)
-# json_ans=json.loads(answer.text)
-# for i in json_ans:
-    # print(i)
 
+duration_text = routes[0]['legs'][-1][u'duration']['text']
+distance_text = routes[0]['legs'][-1][u'distance']['text']
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+fig = plt.figure("caption",figsize=(10,10))
+ax = fig.add_subplot(111)
+# title_text = tr_ru(u"Характерные масштабы задачи управления распределением отходов")
+# ax.set_title(title_text)
+# ax.set_xlabel(tr_ru(u'Время, ч'))
+# ax.set_ylabel(tr_ru(u'Вес, кг'))
+x_coords = [origin['latt'],dest['latt']]
+y_coords = [origin['long'],dest['long']]
+
+annotations=[u"Source",u"Reciever\n%s\n%s" % (,)]
+# annotations=[u"Масштаб\nотходов",u"Масштаб\nперевозок",u"Масштаб\nпроизводственных\nсмен",u"Масштаб\nпроизводственных\nциклов",u"Масштаб\nпланирования\nпроизводства"]
