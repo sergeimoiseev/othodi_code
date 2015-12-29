1ca67acabefe826fbf8538511d8185e5242e3182
sergeimoiseev <moiseev.sergei@gmail.com>
2015-12-29 10:43:31 +0300

    added classes/txt  plus minor tests

--- plot_routes.py
+++ plot_routes.py
@@ -107,14 +107,17 @@ def get_route_from_gmaps(origin,dest):
 
 
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-# import mpld3
 import matplotlib
+# matplotlib.use('nbagg')
+# import matplotlib.pyplot as plt
+# import matplotlib.cm as cm
+# import mpld3
 # matplotlib.use('nbagg')
 
 def plot_route(coord_pairs,annotes):
-    matplotlib.use('nbagg')
+    # matplotlib.use('nbagg')
+    import matplotlib.pyplot as plt
+    import matplotlib.cm as cm
     MIN_L_WIDTH=10
     POINT_SIZE=2*MIN_L_WIDTH
     fig = plt.figure("caption",figsize=(10,10))
@@ -138,35 +141,12 @@ def plot_route(coord_pairs,annotes):
 
 from mpl_toolkits.basemap import Basemap
 
-# def add_points_to_basemap_plot(plt,base_map,added_points_param_list):
-#     MIN_L_WIDTH=7
-#     POINT_SIZE=2*MIN_L_WIDTH
-
-#     if len(added_points_param_list)==0:
-#         print("no points added to plot.")
-#         return plt,base_map
-#     coord_pairs = added_points_param_list[0]
-#     annotes = added_points_param_list[1]
-
-#     x_all=[]
-#     y_all=[]
-#     for i,point in enumerate(coord_pairs):
-#         lon = point[-1]
-#         lat = point[0]
-#         x,y = base_map(*[lon,lat])
-#         x_all.append(x)
-#         y_all.append(y)
-#         if (i!=0 and i!=len(annotes)-1):
-#             plt.annotate(annotes[i], xy=(x,y), xytext=(POINT_SIZE/2,POINT_SIZE/2), textcoords='offset points',bbox=dict(boxstyle="round", fc=(1.0, 0.7, 0.7), ec="none"))
-#     # plt.annotate(annotes[-1], xy=(x_all[-1],y_all[-1]), xytext=(POINT_SIZE/2,POINT_SIZE), textcoords='offset points',bbox=dict(boxstyle="round", fc=(1.0, 0.7, 0.7)))
-#     plt.annotate("annotes[0]", xy=(0,1), textcoords='axes fraction',bbox=dict(boxstyle="round", fc=(1.0, 0.7, 0.7)))
-#     # plt.annotate(annotes[0], xy=(x_all[0],y_all[0]), xytext=(POINT_SIZE/2,POINT_SIZE), textcoords='offset points',bbox=dict(boxstyle="round", fc=(1.0, 0.7, 0.7)))
-#     # plt.plot(x_all,y_all,ls='o',marker='o',ms=POINT_SIZE,lw=MIN_L_WIDTH,alpha=0.5,solid_capstyle='round',color='r')
-#     return plt, base_map
-
-
 def plot_route_on_basemap(coord_pairs,annotes,added_points_param_list=None):
     matplotlib.use('nbagg')
+    import matplotlib.pyplot as plt
+    import matplotlib.cm as cm
+
+    # matplotlib.use('nbagg')
     fig=plt.figure(figsize=(16,12))
     ax=fig.add_axes([0.05,0.05,0.95,0.95])
 
@@ -175,7 +155,7 @@ def plot_route_on_basemap(coord_pairs,annotes,added_points_param_list=None):
     # setup mercator map projection.
     m = Basemap(llcrnrlon=min(lng_list)-2,llcrnrlat=min(lat_list)-2,urcrnrlon=max(lng_list)+2,urcrnrlat=max(lat_list)+2,\
                 rsphere=(6378137.00,6356752.3142),\
-                resolution='h',projection='merc',\
+                resolution='l',projection='merc',\
                 lat_0=0.,lon_0=0.,lat_ts=0.)
     
     MIN_L_WIDTH=7
@@ -227,6 +207,7 @@ def plot_route_on_basemap(coord_pairs,annotes,added_points_param_list=None):
     # draw meridians
     m.drawmeridians(np.arange(-180,180,30),labels=[1,1,0,1])
     # ax.set_title('Great Circle from New York to London')
+    m.bluemarble()
     plt.show()
     # mpld3.show() # bad rendering
 
