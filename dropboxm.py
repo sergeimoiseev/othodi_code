73b9f057f4acf8240199d85f66fe57b6dcd5f2ab
Sergei <moiseev.sergei@gmail.com>
2015-12-31 14:12:07 +0300

    fix to py 3

--- dropboxm.py
+++ dropboxm.py
@@ -16,9 +16,9 @@ class DropboxConnection(object):
 
         # Have the user sign in and authorize this token
         authorize_url = flow.start()
-        print '1. Go to: \n' + authorize_url
-        print '2. Click "Allow" (you might have to log in first)'
-        print '3. Copy the authorization code.'
+        print ('1. Go to: \n%s' % authorize_url)
+        print ('2. Click "Allow" (you might have to log in first)')
+        print ('3. Copy the authorization code.')
         code = raw_input("Enter the authorization code here: ").strip()
         # This will fail if the user enters an invalid authorization code
         access_token, user_id = flow.finish(code)
