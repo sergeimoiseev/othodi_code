3f3b806817b1242f499ad93e0b5875d2519a1823
sergeimoiseev <moiseev.sergei@gmail.com>
2016-01-06 18:27:22 +0300

    graph fastest route now with steps history

--- graph.py
+++ graph.py
@@ -22,24 +22,39 @@ print(pm)
 # finding shortest route
 indices = [(i, j) for i, j in np.ndindex(am.shape)] # list storen in memory
 
-# dt = np.dtype([('way', np.int32, (2,))])
+
 def fastest_route(pm,nodes_num):
-    lambdas_matrix = np.zeros((nodes_num),dtype='f')
-    lm = lambdas_matrix
-    fastest_route = []
+    dt = np.dtype([('edges', np.float32,(nodes_num,2)),('dur', np.int32)])
+    lm = np.zeros((nodes_num),dtype=dt)
+    # lambdas_matrix = np.zeros((nodes_num),dtype='f')
+    # lm = lambdas_matrix
+    # fastest_route = []
     mpm = np.ma.masked_equal(pm, 0.0, copy=False)
     for i in range(1,nodes_num):
-        # print('i=%i' % i)
+        print('i=%i' % i)
         sums = np.empty((0))
         idxs = []
         for ii in range(0,i):
             # print("ii=%i" % ii)
             if mpm[ii][i]  is np.ma.masked:
-                continue
-            sums = np.append(sums,lm[ii]+mpm[ii][i])
+                # continue
+                sums = np.append(sums,np.finfo(np.float64).max)
+            else:
+                sums = np.append(sums,lm[ii]['dur']+mpm[ii][i])
             idxs.append([ii,i])
         i_min = np.argmin(sums)
-        lm[i]=sums[i_min]
-        fastest_route.append(idxs[i_min])
-    return lm, fastest_route
+        lm[i]['dur']=sums[i_min]
+        print('idxs pair to put to lm:'+str(idxs[i_min]))
+        offset = 0
+        print('edges_set')
+        lm[i]['edges'][0] = idxs[i_min]
+        for step in range(nodes_num):
+            print("step = %d" % step)
+            prev_node = lm[i]['edges'][step][0]
+            print(prev_node)
+            if prev_node != 0:
+                print(("  lm[%d]['edges'][0] = "% prev_node)+str( lm[prev_node]['edges'][0]))
+                lm[i]['edges'][step+1] = lm[prev_node]['edges'][0]
+    return lm
+
 print(fastest_route(pm,nodes_num))
